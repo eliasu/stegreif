a66da2572ba0e151036fb3e520444c5d920148b3
Elias <elias@commandg.de>
2022-06-30 18:40:18 +0200

    updated addons / social image generator peak

--- storage/framework/views/48c215bccb63cb95a08c4d95aba323ad5df80c99.php
+++
@@ -1,7 +0,0 @@
-<li class="<?php echo e($item->isActive() ? 'current' : ''); ?>">
-    <a href="<?php echo e($item->url()); ?>">
-        <i><?php echo $item->icon(); ?></i><span><?php echo e(__($item->name())); ?></span>
-        <span class="badge-sm bg-red ml-1"><?php echo e(Statamic\Facades\Stache::duplicates()->count()); ?></span>
-    </a>
-</li>
-<?php /**PATH /Users/elias/Sites/Github/stegreif/vendor/statamic/cms/src/Providers/../../resources/views/nav/duplicates.blade.php ENDPATH**/ ?>
--- storage/framework/views/d2f9f2a7d552423ea26e9a40815bc9bf6abdd6c9.php
+++
@@ -1,7 +0,0 @@
-<li class="<?php echo e($item->isActive() ? 'current' : ''); ?>">
-    <a href="<?php echo e($item->url()); ?>">
-        <i><?php echo $item->icon(); ?></i><span><?php echo e(__($item->name())); ?></span>
-        <updates-badge class="ml-1"></updates-badge>
-    </a>
-</li>
-<?php /**PATH /Users/elias/Sites/Github/stegreif/vendor/statamic/cms/src/Providers/../../resources/views/nav/updates.blade.php ENDPATH**/ ?>
