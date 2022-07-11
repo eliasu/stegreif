87d16113a16648ba11ba1ba18020a95a24ef8c27
Hannes Hoepfner <hello@hanneshoepfner.io>
2022-07-11 22:26:28 +0200

    building up logic and markup

---
+++ app/Http/Livewire/FilterV1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FilterV1 extends Component
+{
+    public function render()
+    {
+        return view('livewire.filter-v1');
+    }
+}
