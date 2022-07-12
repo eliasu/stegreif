7faf28b6cb74b166ed3e8e85ee51e1631764b17e
Hannes Hoepfner <hello@hanneshoepfner.io>
2022-07-12 16:12:29 +0200

    locale set clean php filter

--- app/Http/Livewire/FilterV1.php
+++ app/Http/Livewire/FilterV1.php
@@ -3,14 +3,54 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Statamic\Facades\Entry;
+use Barryvdh\Debugbar\Facades\Debugbar;
+
 
 class FilterV1 extends Component
 {
 
     public $filter = "";
+    public $results;
+    public $tag;
+
+    protected $queryString = [
+        'tag'
+    ];
 
     public function render()
     {
+        // /** @var \Entry $query */
+        // @property Ent
+ 
+        $query = Entry::query()
+        ->where('collection', 'programme')
+        ->where('status', 'published')
+
+        //TODO: locale should be dynamic
+        ->where('locale', 'de')
+
+        // ->where('title', 'like' ,'p%');
+        ->where('title', '=', '#freebrahms');
+
+        // debug($page->locale());
+        debug($query->get());
+        // Debug::debug($query->get('title'));
+
+    // Filter on tag
+    // if (! empty($this->tag)) {
+    //     $query->whereTaxonomyIn(["tags::{$this->tag}"]);
+    // }
+
+    $this->results = $query
+        ->orderBy('date', 'desc')
+        ->get();
+
+    // return view('view');
         return view('livewire.filter-v1');
+        // return view('livewire.filter-v1',  [Debugbar::debug($this)]);
     }
 }
+
+
+
