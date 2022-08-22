d6209f305b5226d05afd9bb80d0f89916a99d7b3
Hannes Hoepfner <hello@hanneshoepfner.io>
2022-08-22 14:56:53 +0200

    dynamic query livewire working

--- app/Http/Livewire/FilterV1.php
+++ app/Http/Livewire/FilterV1.php
@@ -15,12 +15,12 @@ class FilterV1 extends Component
     // this one fucking needs to be private to work with alpine lifewire entagnle
     // public $selection = "";
     public $selection;
-    public $lwResults;
+    // public $lwResults;
 
     // looks like this need to be defined in php to be able to pass properties in antlers
     public $nice;
 
-    public $test= [];
+    // public $test= [];
 
     public $collectionType;
     public $filterTax;
@@ -55,9 +55,8 @@ class FilterV1 extends Component
             // ->whereIn('locale', ["s", "berlin", "test"])
             ;
 
-        debug($query->get());
-        debug("selection", $this->selection);
-
+       
+        
        
 
         // Filter on tag
@@ -65,7 +64,7 @@ class FilterV1 extends Component
         //     $query->whereTaxonomyIn(["tags::{$this->tag}"]);
         // }
 
-        $this->lwResults = $query
+        $results = $query
             ->get()
             ->filter(function ($el, $key) {
                 // return (strlen($this->selection)>0) ? in_array($this->selection, $el->get('tags')) : true;
@@ -73,7 +72,7 @@ class FilterV1 extends Component
                 // debug(is_string($this->selection) ? in_array($this->selection, $el->get('tags')) : true);
                 // debug(in_array($this->selection, $el->get('tags')));
 
-                return is_string($this->selection) ? in_array($this->selection, $el->get('tags')) : true;
+                return strlen($this->selection)>0 ? in_array($this->selection, $el->get('tags')) : true;
                 // return true;
                 //  return in_array($this->selection, $el->get('tags'));
             })
@@ -81,18 +80,27 @@ class FilterV1 extends Component
             ;
             // ->orderBy('date', 'desc')
 
-        $query
-        ->get()
-        ->filter(function ($el, $key) {
-        //    debug($el->get('tags'));
-        //    debug("selection:", $this->selection);
-        //    debug(in_array($this->selection, $el->get('tags')));
 
-        //    debug($key);
-        });
+            debug($query->get());
+            debug($results);
+            debug("selection", $this->selection);
+
+        // $query
+        // ->get()
+        // ->filter(function ($el, $key) {
+        // //    debug($el->get('tags'));
+        // //    debug("selection:", $this->selection);
+        // //    debug(in_array($this->selection, $el->get('tags')));
+
+        // //    debug($key);
+        // });
 
         // return view('view');
-        return view('livewire.filter-v1');
-        // return view('livewire.filter-v1',  [Debugbar::debug($this)]);
+        // return view('livewire.filter-v1');
+
+
+
+
+        return view('livewire.filter-v1',  ['lwResults' => $results]);
     }
 }
