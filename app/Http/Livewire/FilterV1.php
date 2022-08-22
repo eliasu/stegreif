f30842e2c8c9117ca0cf14579ad76c6e8c2cff73
Hannes Hoepfner <hello@hanneshoepfner.io>
2022-08-22 15:23:15 +0200

    cleaned up

--- app/Http/Livewire/FilterV1.php
+++ app/Http/Livewire/FilterV1.php
@@ -4,23 +4,14 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Statamic\Facades\Entry;
-use Barryvdh\Debugbar\Facades\Debugbar;
-
 
 class FilterV1 extends Component
 {
-
-    public $past = true;
-
-    // this one fucking needs to be private to work with alpine lifewire entagnle
-    // public $selection = "";
-    public $selection;
-    // public $lwResults;
-
     // looks like this need to be defined in php to be able to pass properties in antlers
     public $nice;
 
-    // public $test= [];
+    public $past = false;
+    public $selection;
 
     public $collectionType;
     public $filterTax;
@@ -34,72 +25,27 @@ class FilterV1 extends Component
         $this->currentLocale = $config["currLocale"];
     }
 
-  
-    // protected $queryString = [
-    //     'tag'
-    // ];
-
     public function render()
     {
-        // /** @var \Entry $query */
-        // @property Ent
 
         $query = Entry::query()
             ->where('collection', $this->collectionType)
             ->where('status', 'published')
-            ->where('locale', $this->currentLocale)
-            // ->where('tags', 'de')
-
-            // ->whereIn($this->selection, ["hallo", "berlin", "test"])
-            // ->whereIn($this->selection, ["hallo", "berlin", "test"])
-            // ->whereIn('locale', ["s", "berlin", "test"])
-            ;
-
-       
-        
-       
-
-        // Filter on tag
-        // if (! empty($this->tag)) {
-        //     $query->whereTaxonomyIn(["tags::{$this->tag}"]);
-        // }
+            ->where('locale', $this->currentLocale);
 
         $results = $query
             ->get()
-            ->filter(function ($el, $key) {
-                // return (strlen($this->selection)>0) ? in_array($this->selection, $el->get('tags')) : true;
-                // debug("this is the return");
-                // debug(is_string($this->selection) ? in_array($this->selection, $el->get('tags')) : true);
-                // debug(in_array($this->selection, $el->get('tags')));
-
-                return strlen($this->selection)>0 ? in_array($this->selection, $el->get('tags')) : true;
-                // return true;
-                //  return in_array($this->selection, $el->get('tags'));
+            ->filter(function ($el) {
+                // if string == '' treat as is no filter, return always true
+                return strlen($this->selection) > 0 ? in_array($this->selection, $el->get('tags')) : true;
             })
-            // ->all()
-            ;
             // ->orderBy('date', 'desc')
+        ;
 
 
-            debug($query->get());
-            debug($results);
-            debug("selection", $this->selection);
-
-        // $query
-        // ->get()
-        // ->filter(function ($el, $key) {
-        // //    debug($el->get('tags'));
-        // //    debug("selection:", $this->selection);
-        // //    debug(in_array($this->selection, $el->get('tags')));
-
-        // //    debug($key);
-        // });
-
-        // return view('view');
-        // return view('livewire.filter-v1');
-
-
-
+        debug($query->get());
+        debug($results);
+        debug("selection", $this->selection);
 
         return view('livewire.filter-v1',  ['lwResults' => $results]);
     }
