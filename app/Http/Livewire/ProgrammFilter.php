9f3d58521b7cb41b4595259b9433d0fe18cd76cd
Hannes Hoepfner <hello@hanneshoepfner.io>
2022-08-24 12:09:35 +0200

    am einbauen in die templates

--- app/Http/Livewire/ProgrammFilter.php
+++ app/Http/Livewire/ProgrammFilter.php
@@ -10,15 +10,21 @@ class ProgrammFilter extends Component
     public $aktuell = true;
 
     public $selection = [];
-    public $collectionType;
+    public $type;
     public $filterTax;
     public $currentLocale;
 
     public function mount($config)
     {
         debug($config);
-        $this->collectionType = $config["collectionType"];
+
+        if(!isset($config["type"])) return;
+        $this->type = $config["type"];
+
+        if(!isset($config["tax"])) return;
         $this->filterTax = $config["tax"];
+
+        if(!isset($config["currLocale"])) return;
         $this->currentLocale = $config["currLocale"];
 
         if(isset($config["preselection"])) {
@@ -38,22 +44,31 @@ class ProgrammFilter extends Component
 
     public function render()
     {
-
-        // debug(Entry::query()
-        // ->where('collection', $this->collectionType)
-        // ->where('status', 'published'));
-        // debug(Entry::query()
-        // ->where('collection', $this->collectionType)
-        // ->where('status', 'published')
-        // ->where('locale', $this->currentLocale)
-        // ->where('aktuell', false)
-        // ->get());
-
         $query = Entry::query()
-            ->where('collection', $this->collectionType)
+            // ->where('collection', $this->collectionType)
             ->where('status', 'published')
             ->where('locale', $this->currentLocale)
             ->where('aktuell', $this->aktuell);
+
+        switch ($this->type) {
+            case 'reihe':
+                $query = $query->where('collection', 'reihen');
+                break;
+            case 'programm':
+                $query = $query
+                ->where('collection', 'prgramme')
+                ->where('select_type', 'prgramm');
+                break;
+            case 'education':
+                $query = $query  
+                ->where('collection', 'prgramme')
+                ->where('select_type', 'education');
+                break;
+            
+            default:
+                $query = $query->where('collection', 'prgramme');
+                break;
+        }
 
         $results = $query->get();
 
